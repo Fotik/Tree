e855fca9ee656d6ab8321782c5ee2ffeae7d4fb3
cidr5 <nikitos.warnerbros@gmail.com>
2018-03-17 13:49:30 +0300

    Должно работать, но не работает

--- Tree/ViewController.swift
+++ Tree/ViewController.swift
@@ -14,37 +14,52 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var sizeLabel: UILabel!
     
-    var initOffsetX: CGFloat =
-    let initOffsetY = treeImage.frame.origin.y
+    @IBOutlet weak var treeView: UIView!
+    
+    @IBOutlet weak var sizeStepper: UIStepper!
+    
+    var offsetMultiplierX: CGFloat = 0.0
+    var offsetMultiplierY: CGFloat = 0.0
     
     @IBAction func onOffsetValueChange(_ sender: UISlider) {
-        treeImage.frame.origin.x = CGFloat(sender.value) * 50
+        self.treeImage.frame.origin.x = CGFloat(sender.value) * self.offsetMultiplierX
+        self.treeImage.frame.origin.y = CGFloat(sender.value) * self.offsetMultiplierY
     }
     
     @IBAction func onSizeValueChanged(_ sender: UIStepper) {
+        self.treeImage.setValue(sender.value, forKey: "width")
+        self.sizeLabel.setValue(sender.value, forKey: "text")
+        self.calibrateOffsetSlider()
     }
     
     @IBAction func onAngelValueChanged(_ sender: UISegmentedControl) {
+        UIView.animate(withDuration: 0.5) {
+            let angel = Double(sender.titleForSegment(at: sender.selectedSegmentIndex)!)!
+            self.treeImage.transform = CGAffineTransform(rotationAngle: CGFloat(angel/180 * .pi))
+        }
     }
     
     @IBAction func onVisibilitySwitch(_ sender: UISwitch) {
+        self.treeImage.setValue(sender.state.rawValue, forKey: "alpha")
     }
     
     @IBAction func onVisibilityValueChanged(_ sender: UIStepper) {
+        self.treeImage.setValue(sender.value, forKey: "alpha")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        initOffsetX = UIScreen.main.bounds.size.width
-        
+        self.calibrateOffsetSlider()
+        self.sizeLabel.setValue(self.treeImage.frame.width, forKey: "text")
+        self.sizeStepper.setValue(self.treeImage.frame.width, forKey: "value")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func calibrateOffsetSlider()
+    {
+        self.offsetMultiplierX = self.treeView.frame.width - self.treeImage.frame.width
+        self.offsetMultiplierY = self.treeView.frame.height - self.treeImage.frame.height
     }
-
 
 }
 
